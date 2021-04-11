1818c5b552e44319b77267e80e14b944a4ef83a7
Qiru Hu <qiru@cs.ucsb.edu>
2021-04-10 23:58:24 -0400

    Add details and collection view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by qiru hu on 4/10/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
